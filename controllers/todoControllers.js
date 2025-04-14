a1437339975e4bfe473e60df271add2bd12134dc
SayoTeKey <sayo.briefs@gmail.com>
2025-04-14 11:52:40 +0200

    Füge todoModel hinzu und implementiere das Schema sowie das Model für Todos; aktualisiere den createTodoController

--- controllers/todoControllers.js
+++ controllers/todoControllers.js
@@ -1,15 +1,29 @@
-// Diese Datein beinhaltet die Controller-Logik für die Todo-App
-// d.h. ALLE CRUD-Operationen
-// sie braucht die Modell- Infos
+// Diese Datei enthält die Controller-Logik für die Todo-App
+// D.h (das heißt) - ALLE CRUD-Operationen
+// Der braucht die Models Information
+// 1. Model importieren
+// 2. Controller erstellen
+// 3. Exportiere den Controller
+
+import todoModel from "../models/todoModel.js";
 
 // POST - CREATE
 const createTodoController = async (req, res) => {
-    console.log(req.body);
+    console.log(req.body.todo);
+    const neuTodo = req.body.todo;
     try {
-        res.status(201).json({ messge: "Todo erfolgreich erstellt" });
+        // Erstelle ein neues Todo-Objekt mit dem Model
+        const toSave = todoModel({
+            todo: neuTodo,
+        });
+        // Speichere das Todo-Objekt in der Datenbank mit der Methode `save()`
+        await toSave.save();
+        // Sende eine Antwort zurück an den Client mit dem Statuscode 201 (Created)
+        res.status(201).json({ message: "Todo erfolgreich erstellt" });
     } catch (error) {
-        res.status(500).json({ message: error.message })
+        res.status(500).json({ message: error.message });
     }
 };
 
+// Kontrollers exportieren
 export { createTodoController };
